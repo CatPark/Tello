be37b866ea2e71d45b72720480ef600399edfa04
Dx7450D <CatDx7@gmail.com>
2021-12-06 00:29:41 +0800

    Get Tello status, take off and landing

--- tello10.py
+++ tello10.py
@@ -1,4 +1,5 @@
 from djitellopy import tello
+from time import sleep
 import cv2
 
 print("Hello tello")
@@ -6,5 +7,19 @@ print("Hello tello")
 mytello = tello.Tello()
 
 mytello.connect()
+print(f"Tello battery: {mytello.get_battery()}")
+print(f"Tello current state:{mytello.get_current_state()}")
+print(f"Tello TOF: {mytello.get_distance_tof()}")
+print(f"Tello flight time: {mytello.get_flight_time()}")
+print(f"Tello height: {mytello.get_height()}")
+
+mytello.takeoff()
+print(f"Tello height: {mytello.get_height()}")
+sleep(1)
+mytello.land()
+
+print(f"Tello flight time: {mytello.get_flight_time()}")
+
+
 
 
